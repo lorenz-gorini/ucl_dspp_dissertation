fab806672e65b4df5f67b85ce8c28b8fca98c1d4
Lorenzo Gorini <lore.gorini@gmail.com>
2023-07-29 10:57:29 +0200

    Add time-serie plot aggregated by origin country

--- scripts/plot_foreigners_by_dest_area_by_year.py
+++ scripts/plot_foreigners_by_dest_area_by_year.py
@@ -3,8 +3,10 @@ from src.dataset import MicroDataset, TouristOrigin, VariableSubset
 from tqdm import tqdm
 from pathlib import Path
 import pandas as pd
-from bokeh.palettes import Category10
+from bokeh.palettes import Category20, Turbo256, Category10
 import bokeh.plotting as bk
+from bokeh.models import Legend
+
 
 # %%
 region_code_map = pd.read_csv(
@@ -29,8 +31,6 @@ for macro_area, region_codes_single_area in macro_area_to_region_codes.items():
 
 # Draw two bokeh plots for italian and foreigners to show how their expenses
 # and number of trips changed over the years divided by country or by italian region
-our_total_df = []
-
 total_dfs_by_year = []
 for year in tqdm(range(1997, 2023, 1)):
     df_primary = MicroDataset(
@@ -104,7 +104,90 @@ for i, macro_area in enumerate(["North", "Center", "South"]):
     )
 f.legend.location = "top_left"
 f.legend.click_policy = "hide"
-bk.output_file(
-    "/mnt/c/Users/loreg/Documents/dissertation_data/figures/expenses_by_area.html"
+
+# %%
+# Plot the same time series after grouping the travellers by their country of origin
+
+# 1. Load the country codes
+country_codes = pd.read_csv(
+    "/mnt/c/Users/loreg/Documents/dissertation_data/Metadati-CSV/TSTATI.csv", sep=";"
 )
+# turn the country codes into a dictionary
+country_code_to_country_name = {}
+for _, row in country_codes.iterrows():
+    country_code_to_country_name[row["ELEM_DOMINIO"]] = row["D_EL_DOMINIO"]
+
+# 2. Load the data by year and compute the aggregated values by year and
+# by country of origin
+# TODO: Map the codes in "STATO_RESIDENZA" column and then groupby it
+total_dfs_by_year = []
+for year in tqdm(range(1997, 2023, 1)):
+    df_primary = MicroDataset(
+        variable_subset=VariableSubset.PRIMARY,
+        tourist_origin=TouristOrigin.FOREIGNERS,
+        year=year,
+        destination_folder=Path("/mnt/c/Users/loreg/Documents/dissertation_data/raw"),
+    ).df
+
+    df_primary["origin_country"] = df_primary["STATO_RESIDENZA"].map(
+        country_code_to_country_name
+    )
+    print(
+        "The country codes not associated to a country name are: "
+        + str(df_primary["origin_country"].isna().sum())
+    )
+    totals_single_area = df_primary.groupby("origin_country")[
+        ["FPD_NOTTI", "FPD_SPESA_FMI", "FPD_VIAG"]
+    ].sum()
+    totals_single_area.reset_index(drop=False, inplace=True)
+    totals_single_area["year"] = year
+
+    total_dfs_by_year.append(totals_single_area)
+
+totals_by_year = pd.concat(total_dfs_by_year, axis=0)
+totals_by_year.sort_values(by=["year", "origin_country"], inplace=True)
+totals_by_year.head()
+
+# %%
+# Draw two bokeh plots for foreigners to show how their expenses
+# and number of trips changed over the years divided by country of origin
+f = bk.figure(
+    title=f"Total amount spent by foreigners in Italy by country of origin",
+    x_axis_label="Year",
+    y_axis_label="Total Expenses",
+    x_range=(1997, 2023),
+    y_range=(0, max(totals_by_year["FPD_SPESA_FMI"]) * 1.1),
+    tooltips=[
+        ("Year", "@year"),
+        ("Expenses", "@FPD_SPESA_FMI"),
+        ("Country", "@origin_country"),
+    ],
+)
+# Filter on the top 20 countries to improve readability
+top_20_series = (
+    totals_by_year.groupby("origin_country")["FPD_SPESA_FMI"]
+    .sum()
+    .sort_values(ascending=False)
+    .head(20)
+    .index
+)
+
+# Line plot for the total nights spent by foreigners in Italy colored by macroarea
+legend_it = []
+for i, country in enumerate(top_20_series):
+    palette = Category20[len(top_20_series)] if len(top_20_series) <= 20 else Turbo256
+    country_serie = f.line(
+        x="year",
+        y="FPD_SPESA_FMI",
+        color=palette[i],
+        source=totals_by_year[totals_by_year["origin_country"] == country],
+        line_width=2,
+    )
+    legend_it.append((country, [country_serie]))
+legend = Legend(items=legend_it)
+legend.click_policy = "mute"
+
+f.add_layout(legend, "right")
+bk.output_notebook()
 bk.show(f)
+
